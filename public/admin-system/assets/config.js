ba6afba8def8e3006e013476af7321d20c2cd874
YALERIMA TECHNOLOGIES LIMITED <yalerimatechnologies@gmail.com>
2026-09-10 23:13:47 +0100

    Fix admin authorization UUID

--- public/admin-system/assets/config.js
+++ public/admin-system/assets/config.js
@@ -6,5 +6,5 @@ window.YALERIMA_CONFIG = {
     "sb_publishable_Zz1GZKarl9DiRlAoTzI6MQ_1uZ5OWEZ",
 
   adminUserId:
-    "f644751b-9059-4715-badb-5e4ce6a30be3"
+    "8b1a80b7-515d-4744-94c0-579b9e87ce59"
 };
